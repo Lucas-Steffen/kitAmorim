3b8ba9379e20c5a8c5553d961afd652a83c2aa7d
Lucas-Steffen <lucasgabriel.programador@gmail.com>
2026-05-28 19:41:32 -0400

    feat: add environment configuration example and restructure backend modules for improved organization and clarity

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,50 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app/app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe } from "@nestjs/common";
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from "path";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      whitelist: true,
+      forbidNonWhitelisted: false,
+    }),
+  );
+
+  app.enableCors({
+    origin: process.env.ORIGIN!,
+  });
+
+  app.useStaticAssets(join(process.cwd(), 'uploads'), {
+    prefix: '/uploads/',
+  });
+
+  if (process.env.NODE_ENV === "development") {
+    const config = new DocumentBuilder()
+      .setTitle("KitAmorim")
+      .setDescription("API")
+      .setVersion("1.0")
+      .addBearerAuth(
+        {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+          in: "header",
+        },
+        "Authorization"
+      )
+      .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config, {
+      operationIdFactory: (_controllerKey, methodKey) => methodKey,
+    });
+    documentFactory.security = [{ Authorization: [] }];
+    SwaggerModule.setup("docs", app, documentFactory);
+  }
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
